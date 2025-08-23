743ed2524601e7348d0d68fa8103694d6a226236
ottomato <11212713+ottomato@users.noreply.github.com>
2025-08-23 13:13:53 +0000

    Add more stuff

--- src/Character/Base.php
+++ src/Character/Base.php
@@ -4,15 +4,5 @@ namespace App\Character;
 
 class Base
 {
-    private string $name;
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): void
-    {
-        $this->name = $name;
-    }
+    public string $name;
 }
