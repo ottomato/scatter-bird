10b22ee1fd53c7e2ea31bb1ace37072da3eb4cd4
ottomato <11212713+ottomato@users.noreply.github.com>
2025-09-22 19:22:38 +0000

    Add world builder

--- src/World/WorldBuilder.php
+++ src/World/WorldBuilder.php
@@ -14,8 +14,8 @@ class WorldBuilder
         $player = new Base();
         $player->name = $name;
 
-        $bedroom = new Location('bedroom', 'a messy bedroom', $player);
-        $hallway = new Location('hallway', 'a narrow hallway', $player);
+        $bedroom = new Location('bedroom', 'messy bedroom', $player);
+        $hallway = new Location('hallway', 'narrow hallway', $player);
 
         $bedroom->addAccessWay($hallway->id);
         $hallway->addAccessWay($bedroom->id);
